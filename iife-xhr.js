eb5ff6f6570f050ae3003c03e5f60c6c9f6bcd33
Thomas J. Shavor <thomasshavor@gmail.com>
2016-07-27 10:19:59 -0500

    steps 5 and 6 complete

--- iife-xhr.js
+++ iife-xhr.js
@@ -1,1 +1,22 @@
 console.log ("Hello World");
+
+function showCarnivores (carnivores) {
+	var list= document.querySelector('.carnivores')
+
+	carnivores.forEach(function (carnivore){
+		list.innerHTML += `<li>carnivore</li>`
+
+	})
+	}
+
+function showHerbivores (herbivores) {
+var list= document.querySelector('.herbivores')
+
+	herbivores.forEach(function (herbivores){
+		list.innerHTML += `<li>herbivores</li>`
+
+	})
+}
+
+Predator.getCarnivores(showCarnivores);
+Predator.getHerbivores(showHerbivores);
