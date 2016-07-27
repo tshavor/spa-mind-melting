b2ba78433d29970d3dbab603b5b2e277230dcac3
Thomas J. Shavor <thomasshavor@gmail.com>
2016-07-27 10:08:42 -0500

    step 4 completed

--- predator.js
+++ predator.js
@@ -2,4 +2,47 @@ var Predator = (function (){
 	var carnivore = [];
 	var herbivore = [];
 
+return {
+	getCarnivores: function (callback) {
+		// get the data (XHR-Http request)
+		var xhr= new XMLHttpRequest()
+		xhr.open('GET', 'carnivores.json')
+			xhr.addEventListener('load', function(event){
+		// store it in a private variable
+				carnivores= JSON.parse(event.target.responseText)
+				callback(carnivores)
+
+			})
+			xhr.send()
+
+		// execute the callback
+
+	},
+	getHerbivores: function (callback) {
+
+		// get the data (XHR-Http request)
+		var xhr= new XMLHttpRequest()
+		xhr.open('GET', 'herbivores.json')
+			xhr.addEventListener('load', function(event){
+		// store it in a private variable
+				herbivores= JSON.parse(event.target.responseText)
+				callback(herbivores)
+
+			})
+			xhr.send()
+
+	},
+
+}
+
+
+
+
+
+
+
+
+
+
+
 }())
